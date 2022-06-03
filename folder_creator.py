7b08dcca57af09db299f13db94bd7f3851bb9d7c
mdecosto <mdecosto@futurex.ph>
2022-06-03 15:35:07 +0800

    initial commit

---
+++ folder_creator.py
@@ -0,0 +1,15 @@
+from importlib.resources import path
+import os
+
+day = "D019 Intermediate - Instances, State and Higher Order Functions"
+foldername = ""
+
+for i in day:
+  if i == " ":
+    foldername += "_"
+  else:
+    foldername += i
+    
+path =  f"/Users/mdecosto/Desktop/study/udemy/100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2022/{foldername}"
+
+os.mkdir(path)
