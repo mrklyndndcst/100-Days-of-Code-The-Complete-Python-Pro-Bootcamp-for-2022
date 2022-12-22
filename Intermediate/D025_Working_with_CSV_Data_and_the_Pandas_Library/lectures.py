133529e8b1b51d285be962f30ff3f626fde3f358
mdecosto <mdecosto@futurex.ph>
2022-12-22 22:13:56 +0800

    D25 Lecture 01

--- Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/lectures.py
+++ Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/lectures.py
@@ -2,24 +2,21 @@
 #   data = data_file.readlines()
 #   print(data)
 
-
-
 # import csv
-
-# temperature = []
 
 # with open("Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/weather_data.csv") as data_file:
 #   data = csv.reader(data_file)
+#   temperature = []
 #   for row in data:
 #     if row[1].isnumeric():
 #       temperature.append(int(row[1]))
-
-# print(temperature)
-
+#   print(temperature)
 
 import pandas
 
 # data = pandas.read_csv("Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/weather_data.csv")
+# print(type(data)) #Series
+# print(data["temp"]) 
 
 # data_dict = data.to_dict()
 # print(data_dict)
@@ -34,19 +31,21 @@ import pandas
 # print(max_temp)
 
 # # get data in columns
-# print(data["conditions"])
+# print(data["condition"])
 # print(data.condition)
 
-# # get data in Row
-# print(data[data.temp == "Monday"])
+# # get data in Row by the request day
+# print(data)
+# print(data[data.day == "Monday"])
 
 # # get data from max temp 
 # print(data[data.temp == max_temp])
 
+# # get the condition in specific row
 # monday = data[data.day == "Monday"]
 # print(monday.condition)
 
-# monday_temp_F = int(monday.temp) * 1.8 + 32
+# monday_temp_F = int(monday.temp) * 9/5 + 32
 # print(monday_temp_F)
 
 # data_dict = {
@@ -54,7 +53,7 @@ import pandas
 #   "scores": [76, 56, 65],
 # }
 # data = pandas.DataFrame(data_dict)
-# data.to_csv("new_data.csv")
+# data.to_csv("Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/new_data.csv")
 
 data = pandas.read_csv("Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 gray = len(data[data["Primary Fur Color"] == "Gray"])
@@ -67,4 +66,4 @@ data_dict = {
 }
 
 new_data = pandas.DataFrame(data_dict)
-new_data.to_csv("squirrel_count.csv")
+new_data.to_csv("Intermediate/D025_Working_with_CSV_Data_and_the_Pandas_Library/squirrel_count.csv")
